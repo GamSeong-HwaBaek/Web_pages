f4b65e486937dc21265f6fa3cb9010e3d4ab502b
choi <ex3442@naver.com>
2022-04-03 04:05:40 +0900

    add front pages

---
+++ frontpage.js
@@ -0,0 +1,21 @@
+function createNewAccount() {
+    console.log("create new account");
+}
+function findIDorPassword() {
+    console.log("findIDorPassword");
+}
+function signIn() {
+    console.log("signIn");
+}
+function relateToGoogle() {
+    console.log("relate1");
+}
+function relateToGit() {
+    console.log("relate1");
+}
+function relateToNaver() {
+    console.log("relate1");
+}
+function relateToKakao() {
+    console.log("relate1");
+}
