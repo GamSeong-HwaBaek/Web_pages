7faec692f8bc7c8aecac3c2c0b86bb6a76d08db3
es3442 <es3442@naver.com>
2022-04-07 23:10:24 +0900

    exhibite change

--- client/exhibit.js
+++ client/exhibit.js
@@ -1,19 +1,3 @@
-let xmark = document.getElementById("xmark");
-xmark.addEventListener("mouseover", function (event) {
-    //highlight the mouseover target
-    event.target.className = "fa-solid fa-circle-xmark fa-2x";
-    event.target.style.color = "whitesmoke";
-}, false);
-
-xmark.addEventListener("mouseout", function (event) {
-    event.target.className = "fa-regular fa-circle-xmark fa-2x";
-    event.target.style.color = "whitesmoke";
-}, false);
-
-function click_xmark() {
-    console.log('close');
-}
-
 let trashcan = document.getElementById('trashcan');
 trashcan.addEventListener("mouseover", function (event) {
     //highlight the mouseover target
