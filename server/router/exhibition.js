92a8d733adf561abc8047208483c50c84c38cb31
choi <es3442@naver.com>
2022-05-29 18:38:06 +0900

    change exhibition pages

--- server/router/exhibition.js
+++ server/router/exhibition.js
@@ -1,22 +1,33 @@
 import express from 'express';
 import 'express-async-errors';
+import { body } from 'express-validator';
 import * as exhibitionController from '../controller/exhibition.js';
+import { isAuth } from '../middleware/auth.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+
+const validateExhibition = [
+    body('title')
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage('text should be at least 3 characters'),
+    validate,
+];
 //GET /exhibition
 //GET /exhibition?username=:username 사용자에 대한 모든 다이어리 가져오기.
-//router.get('/', exhibitionController.getAll);
+router.get('/', exhibitionController.getExhibitions);
 
 //GET /exhibition/id 전시 선택
 router.get('/:id', exhibitionController.getbyId);
 
 //POST /exhibition
-router.post('/', exhibitionController.create);
+router.post('/', validateExhibition, exhibitionController.create);
 
 //PUT /exhibition/:id
-router.put('/:id', exhibitionController.update);
+//router.put('/:id', exhibitionController.update);  
 
 //DELETE /exhibition/:id
-router.delete('/:id', exhibitionController.remove);
+//router.delete('/:id', exhibitionController.remove);
 
 export default router;
