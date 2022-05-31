1f23a25978bc762bb888a42a4697764122b2c2ca
Haeun0415 <haeun8474@gmail.com>
2022-05-31 14:53:21 +0900

    데이터 로컬 저장

--- client/public/js/tempdiary.js
+++ client/public/js/tempdiary.js
@@ -1,6 +1,25 @@
 var submit = document.getElementById('submitButton');
 submit.onclick = showImage;     //Submit 버튼 클릭시 이미지 보여주기
 
+/* 버튼 */
+const btnSLD = document.querySelector('.btnSetLocalData');
+/* input 텍스트 */
+const setInput = document.querySelector('#setInput');
+
+function setData(){
+    /* 할 일 처리 --> 입력한 텍스트 값 가져오기 */
+    let setInputValue = setInput.value;
+
+    /* localStorage 저장 */
+    localStorage.setItem('inputValue', setInputValue);
+    setInput.value = '';
+
+    /* set Data 버튼 클릭 시 */
+    btnSLD.addEventListener('click', setData);
+};
+
+
+
 function showImage() {
     var newImage = document.getElementById('image-show').lastElementChild;
     newImage.style.visibility = "visible";
